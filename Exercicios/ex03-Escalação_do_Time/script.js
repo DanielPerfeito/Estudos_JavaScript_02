d8e687c29bc3003d79b2de2d610f7686f7b5f166
DanielPerfeito <dp.ribeiro.1996@gmail.com>
2023-12-08 23:40:21 -0300

    Exercicio 03 - Escalação de Time

--- Exercicios/ex03-Escalação_do_Time/script.js
+++ Exercicios/ex03-Escalação_do_Time/script.js
@@ -1,27 +1,54 @@
+// Função para escalar um jogador:
+function escalarJogador() {
+    // Obter os valores dos campos de entrada (posicao, nome, numero)
+    const posicao = document.getElementById('posicao').value;
+    const nome = document.getElementById('nome').value;
+    const numero = document.getElementById('numero').value;
 
+    // Obter a lista de jogadores do time
+    const listaTime = document.getElementById('listaTime');
 
-//Função para escalar um jogador:
-function escalarJogador(){
+    // Exibir uma confirmação para o usuário
+    const confirmacao = confirm(`Escalar o Jogador ${nome} (${numero}) como ${posicao}?`);
 
-    const jogador = {
-        posicao: document.getElementById('posicaoJogador').value,
-        nome: document.getElementById('nomeJogador').value,
-        camisa: document.getElementById('camisaJogador').value
+    // Se o usuário confirmar, adicionar o jogador à lista
+    if (confirmacao === true) {
+        // Criar um novo elemento de lista (li) para o jogador
+        const itemJogador = document.createElement('li');
+
+        // Definir o ID do elemento para identificação futura
+        itemJogador.id = `jogador-${numero}`;
+
+        // Definir o texto interno do elemento com informações do jogador
+        itemJogador.innerText = `${posicao} - ${nome} (${numero})`;
+
+        // Adicionar o elemento à lista de jogadores do time
+        listaTime.appendChild(itemJogador);
     }
 
-    const listaEscalacao = document.getElementById('escalacao')
-
-    const itemLista = document.createElement('li')
-    itemLista.innerText = `Nome: ${jogador.nome} | Camisa: ${jogador.camisa} | Posição: ${jogador.posicao}`
-    listaEscalacao.appendChild(itemLista)
-
-    console.log(itemLista)
+    // Limpar os campos de entrada após a escalação
+    document.getElementById('posicao').value = "";
+    document.getElementById('nome').value = "";
+    document.getElementById('numero').value = "";
 }
 
-// Função para Limpar os campos de escalação.
-function limparEscalacao(){
+// Função para desescalar um jogador
+function desescalarJogador() {
+    // Obter o número do jogador a ser removido
+    const numero = document.getElementById('numeroParaRemover').value;
 
-    document.getElementById('posicaoJogador').value = "";
-    document.getElementById('nomeJogador').value = "";
-    document.getElementById('camisaJogador').value = "";
+    // Obter o elemento do jogador a partir do seu ID
+    const jogadorParaRemover = document.getElementById(`jogador-${numero}`);
+
+    // Exibir uma confirmação para o usuário
+    const confirmacao = confirm(`Remover o  ${jogadorParaRemover.innerText}?`);
+
+    // Se o usuário confirmar, remover o jogador da lista
+    if(confirmacao){
+        // Obter a lista de jogadores do time e remover o jogador específico
+        document.getElementById('listaTime').removeChild(jogadorParaRemover);
+
+        // Limpar o campo de entrada após a remoção
+        document.getElementById("numeroParaRemover").value = "";
+    }
 }
